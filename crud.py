adf4aa4d613c6cb1617af1587bf01d358ee959ff
Hannah Paxton <hannahlinnpaxton@gmail.com>
2021-05-28 04:02:58 +0000

    End of 5/28 lab

--- crud.py
+++ crud.py
@@ -23,6 +23,11 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
@@ -33,9 +38,30 @@ def create_rating(user, movie, score):
 
     return rating
 
+def get_movie_by_id(movie_id):
+    """Return all movies by id."""
 
+    return Movie.query.get(movie_id)
 
+def get_users():
+    "View all users"
 
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Showing details of users"""
+
+    return User.query.get(user_id)
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
+def get_user_by_password(email,password):
+    """Return a user by password."""
+
+    return User.query.filter(User.password == password, User.email == email).first()
 
 if __name__ == '__main__':
     from server import app
